c72b9d031bd8e14e630c083ca560ccb3208de1ed
humanchen <chensiyu@chensiyudeMac-mini.local>
2017-03-08 16:38:33 +0800

    update

--- App/Login/Login.js
+++ App/Login/Login.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    TextInput,
+    Button,
+    TouchableOpacity
 } from 'react-native';
 
 import TopNavigator from '../Common/TopNavigator'
+
+
+var Dimensions = require('Dimensions');
+var ScreenWidth = Dimensions.get('window').width;
 
 export default class Login extends Component {
     componentWillMount() {
 
     }
+
     _leftItemAction() {
         console.log('左侧按钮点击了');
     }
@@ -20,20 +28,42 @@ export default class Login extends Component {
         console.log('右侧按钮点击了');
     }
 
+
+    onPress() {
+        Alert.alert('Button has been pressed!');
+    }
+
     render() {
         return (
 
             <View style={styles.container}>
                 <TopNavigator
                     title='登录'
-
                     rightItemTitle='注册'
-                    rightTextColor='#3393F2'
+                    rightTextColor='red'
                     leftItemFunc={this._leftItemAction.bind(this)}
                     rightItemFunc={this._rightItemAction.bind(this)}/>
                 <View style={styles.top}>
-             <Text>sdfsdf</Text>
-            </View>
+                    <View style={styles.inputContainer}>
+                        <View style={styles.inputBox}>
+                            <Text style={styles.label}>手机号</Text>
+                            <TextInput style={styles.inputText} placeholder='请输入手机号'></TextInput>
+                        </View>
+                        <View style={styles.line}></View>
+                        <View style={styles.inputBox}>
+                            <Text style={styles.label}>密码</Text>
+                            <TextInput style={styles.inputText} placeholder='请输入密码'></TextInput>
+                        </View>
+                    </View>
+                    <View style={styles.forgetPassword}>
+                        <TouchableOpacity
+
+                            onPress={() => this.jumpToMain()}>
+                          <Text style={{color:'blue',fontSize:14}}>忘记密码?</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </View>
                 <View style={styles.bottom}>
                     <Text>sdfsdf</Text>
                 </View>
@@ -48,18 +78,54 @@ const styles = StyleSheet.create({
 
         // justifyContent: 'center',
         // alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: '#e6e6e6',
         flexDirection: 'column'
     },
 
     top: {
+
         flex: 1,
-        backgroundColor:'red',
-        flexDirection:'column',
-        justifyContent:'flex-start'
+        flexDirection: 'column',
+        justifyContent: 'flex-start'
     },
     bottom: {
-        flex: 1,backgroundColor:'blue',flexDirection:'column',justifyContent:'flex-end'
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end'
+    },
+    inputContainer: {
+        height: 81,
+        backgroundColor: 'white',
+        marginTop: 20,
+    },
+    inputBox: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 40,
+        alignItems: 'center',
+
+
+    },
+    label: {
+        width: 60,
+        textAlign: 'left',
+        marginLeft: 10,
+    },
+    line: {
+        backgroundColor: '#e6e6e6',
+        height: 1,
+        width: ScreenWidth,
+    },
+    inputText:{
+        width:ScreenWidth-60-10-10,
+        height:30,
+        alignSelf:'center',
+        fontSize: 15,
+    },
+    forgetPassword:{
+        alignSelf:'flex-end',
+        margin: 10,
+
     },
     welcome: {
         fontSize: 20,
