04ed71a2238afbd9e1030e7d0a774cfaa0c31d10
humanchen <human@HumandeMacBook-Air.local>
2017-03-16 15:14:03 +0800

    update
    format

--- App/FirstPage/SwipeCell.js
+++ App/FirstPage/SwipeCell.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import Swiper from 'react-native-swiper'
 var Dimensions = require('Dimensions');
 var ScreenWidth = Dimensions.get('window').width;
@@ -12,110 +7,114 @@ var ScreenHeight = Dimensions.get('window').height;
 
 export default class SwipeCell extends Component {
 
-  constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
 
-      this.state = {
-          img1:'',
-          img2:'',
-          img3:'',
-      };
-  }
+        this.state = {
+            img1: '',
+            img2: '',
+            img3: '',
+            imag4: ''
+        };
+    }
 
-  componentDidMount(){
-    let formData = new FormData();
-    formData.append("device_id","C4BBE917-20FF-44E6-844A-0628EC6C096B");
-    formData.append("access_token","a99d5c1c-c893-49e8-bb8b-de2dd4016466");
-    formData.append("outlet_no","096");
-    fetch('https://lotusgo.cplotus-gz.com/api-1.8/home/advertise' , {
-        method: 'POST',
-        headers: { "Content-Type": "application/json;charset=UTF-8"},
-        body: formData
-    }).then((response) => {
-        if (response.ok) {
-            return(response.json());
+    componentDidMount() {
+        let formData = new FormData();
+        formData.append("device_id", "C4BBE917-20FF-44E6-844A-0628EC6C096B");
+        formData.append("access_token", "a99d5c1c-c893-49e8-bb8b-de2dd4016466");
+        formData.append("outlet_no", "096");
+        fetch('https://lotusgo.cplotus-gz.com/api-1.8/home/advertise', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json;charset=UTF-8"
+            },
+            body: formData
+        }).then((response) => {
+            if (response.ok) {
+                return (response.json());
 
-        }
-    }).then((json) => {
-        // alert(json.message);
-        // alert(JSON.stringify(json));
-        let data1 = json.data;
-        // alert(data1)
+            }
+        }).then((json) => {
+            // alert(json.message);
+            // alert(JSON.stringify(json));
+            let data1 = json.data;
+            // alert(data1)
 
+            this.setState({img1: data1.focus_list[0].image_url, img2: data1.focus_list[1].image_url, img3: data1.focus_list[2].image_url, img4: data1.focus_list[3].image_url});
+            // this.refs.toast.show(this.state.dataList[0].select_icon, DURATION.LENGTH_LONG);
+            // this.refs.img1.setNativeProps({
+            //    source:require('../Image/star123.png')
+            // });
 
-        this.setState({
-        img1:data1.focus_list[0].image_url,
-        img2:data1.focus_list[1].image_url,
-        img3:data1.focus_list[2].image_url,
+        }).catch((error) => {
+            console.error(error);
         });
-        // this.refs.toast.show(this.state.dataList[0].select_icon, DURATION.LENGTH_LONG);
-        // this.refs.img1.setNativeProps({
-        //    source:require('../Image/star123.png')
-        // });
+    }
 
-    }).catch((error) => {
-        console.error(error);
-    });
-  }
+    render() {
+        return (
+            <Swiper style={styles.wrapper} height={180} loop={true} autoplay={true} activeDot={< View style = {{backgroundColor: 'white', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}}/>}>
+                <View style={styles.slide1}>
 
-  render() {
-    return (
-      <Swiper style={styles.wrapper}
-       height={200}
-       loop={true}
-       autoplay={true}
-       activeDot={<View style={{backgroundColor: 'white', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />}
-        >
-       <View style={styles.slide1}>
-         {/* <Text style={styles.text}>Hello Swiper</Text> */}
-         <Image  source={{url:this.state.img1}} style={styles.tabIcon}/>
-       </View>
-       <View style={styles.slide2}>
-         {/* <Text style={styles.text}>Beautiful</Text> */}
-         <Image  source={{url:this.state.img2}} style={styles.tabIcon}/>
-       </View>
-       <View style={styles.slide3}>
-         {/* <Text style={styles.text}>And simple</Text> */}
-         <Image  source={{url:this.state.img3}} style={styles.tabIcon}/>
-       </View>
-     </Swiper>
-    );
-  }
+                    <Image source={{
+                        url: this.state.img1
+                    }} style={styles.tabIcon}/>
+                </View>
+                <View style={styles.slide2}>
+
+                    <Image source={{
+                        url: this.state.img2
+                    }} style={styles.tabIcon}/>
+                </View>
+                <View style={styles.slide3}>
+
+                    <Image source={{
+                        url: this.state.img3
+                    }} style={styles.tabIcon}/>
+                </View>
+                <View style={styles.slide3}>
+
+                    <Image source={{
+                        url: this.state.img4
+                    }} style={styles.tabIcon}/>
+                </View>
+            </Swiper>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-    wrapper: {
- },
- slide1: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#9DD6EB',
- },
- slide2: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#97CAE5',
- },
- slide3: {
-   flex: 1,
-   justifyContent: 'center',
-   alignItems: 'center',
-   backgroundColor: '#92BBD9',
- },
- text: {
-   color: '#fff',
-   fontSize: 30,
-   fontWeight: 'bold',
- },
- tabIcon: {
-    width: ScreenWidth,
-    height: 200,
-    // resizeMode: 'stretch',
-   //  marginTop: 12.5
-}
+    container: {
+        flex: 1
+    },
+    wrapper: {},
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DD6EB'
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#97CAE5'
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    tabIcon: {
+        width: ScreenWidth,
+        height: 180,
+        resizeMode: 'stretch',
+        //  marginTop: 12.5
+    }
 });
--- App/My/My.js
+++ App/My/My.js
@@ -1,12 +1,11 @@
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  View,
-  Text,
-  StyleSheet,
-  ListView,
-  TouchableOpacity,
-  Image
+    View,
+    Text,
+    StyleSheet,
+    ListView,
+    TouchableOpacity,
+    Image
 } from 'react-native';
 import NameCell from './NameCell'
 import MemberCardCell from './MemberCardCell'
@@ -16,119 +15,120 @@ const DI = require('Dimensions');
 const WINDOW = DI.get('window');
 
 export default class My extends Component {
-  constructor(props) {
-    super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2,
-      sectionHeaderHasChanged:(s1,s2) => s1!=s2});
-    this.state = {
-        dataSource: ds.cloneWithRowsAndSections({0:['a', 'b'],1:['a', 'b'],2:['分享给好友', '用户反馈','我的优惠券','关于我们','设置']}),  // cloneWithRows 放置数组
-        // loaded: false,
-    };
+    constructor(props) {
+        super(props);
+        const ds = new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+            sectionHeaderHasChanged: (s1, s2) => s1 != s2
+        });
+        this.state = {
+            dataSource: ds.cloneWithRowsAndSections({
+                0: [
+                    'a', 'b'
+                ],
+                1: [
+                    'a', 'b'
+                ],
+                2: ['分享给好友', '用户反馈', '我的优惠券', '关于我们', '设置']
+            }), // cloneWithRows 放置数组
+            // loaded: false,
+        };
 
-
-  }
-
-  // 返回具体的cell
-  _renderRow(rowData,sectionID,rowID) {
-      if(rowID==0&&sectionID==0){
-        return(
-          <NameCell/>
-        );
-
-      }else  if(rowID==1&&sectionID==0){
-
-        return(
-          <View>
-          <MemberCardCell/>
-          <View style={styles.cellbreak}>
-          </View>
-          </View>
-        );
-
-
-      }else if (sectionID==1&&rowID==0) {
-        return(
-          <Order1Cell/>
-        );
-      }else if (sectionID==1&&rowID==1) {
-        return(
-          <View>
-              <Order2Cell/>
-              <View style={styles.cellbreak}>
-              </View>
-          </View>
-
-        );
-      }
-
-      else
-      {
-      return(
-          <TouchableOpacity activeOpacity={0.5} onPress={ () => this.pushToDetail(rowData) }>
-              <View style={styles.cellView}>
-                <View>
-                    <Text style={styles.textS}>{rowData}</Text>
-                </View>
-                <View style={{flexDirection:'row',alignItems: 'center',}} >
-                    {/* <Text style={styles.textS}>查看全部订单</Text> */}
-                    <Image style={styles.imageStyle}  source={require('../Image/arrowright.png')}
-                  />
-                </View>
-              </View>
-          </TouchableOpacity>
-      );
     }
-  }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {/* <Text >I'm the My component</Text> */}
-        <ListView showsVerticalScrollIndicator={false}
-            dataSource={this.state.dataSource}  // 数据源
-            renderRow={this._renderRow.bind(this)}
-        />
-      </View>
-    );
-  }
+    // 返回具体的cell
+    _renderRow(rowData, sectionID, rowID) {
+        if (rowID == 0 && sectionID == 0) {
+            return (<NameCell/>);
+
+        } else if (rowID == 1 && sectionID == 0) {
+
+            return (
+                <View>
+                    <MemberCardCell/>
+                    <View style={styles.cellbreak}></View>
+                </View>
+            );
+
+        } else if (sectionID == 1 && rowID == 0) {
+            return (<Order1Cell/>);
+        } else if (sectionID == 1 && rowID == 1) {
+            return (
+                <View>
+                    <Order2Cell/>
+                    <View style={styles.cellbreak}></View>
+                </View>
+
+            );
+        } else {
+            return (
+                <TouchableOpacity activeOpacity={0.5} onPress={() => this.pushToDetail(rowData)}>
+                    <View style={styles.cellView}>
+                        <View>
+                            <Text style={styles.textS}>{rowData}</Text>
+                        </View>
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                            {/* <Text style={styles.textS}>查看全部订单</Text> */}
+                            <Image style={styles.imageStyle} source={require('../Image/arrowright.png')}/>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            );
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+             < ListView showsVerticalScrollIndicator = {false}
+            dataSource = {
+                this.state.dataSource
+            } // 数据源
+            renderRow = {
+                this._renderRow.bind(this)
+            }
+            />
+          </View>
+        );
+    }
 }
 
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  cellView: {
-    flexDirection:'row',
-    justifyContent:'space-between',
-     backgroundColor:'white',
-     height:30,
-     borderBottomWidth:1,
-     borderBottomColor:'#e8e8e8',
-     padding:10,
-  },
-  textS:{
-    fontSize:12
-  },
-  imageStyle: {
-      width: 15,
-      height: 15
-  },
-  topTitle: {
-      color:'red',
-      fontSize:15,
-      width:100,
-      marginBottom:8
-  },
-  bottomTitle: {
-      color:'blue',
-  },
-  cellbreak:{
-    height:10,
-
-  }
+    container: {
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+    },
+    cellView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        height: 30,
+        borderBottomWidth: 1,
+        borderBottomColor: '#e8e8e8',
+        padding: 10
+    },
+    textS: {
+        fontSize: 12
+    },
+    imageStyle: {
+        width: 15,
+        height: 15
+    },
+    topTitle: {
+        color: 'red',
+        fontSize: 15,
+        width: 100,
+        marginBottom: 8
+    },
+    bottomTitle: {
+        color: 'blue'
+    },
+    cellbreak: {
+        height: 10
+    }
 });
